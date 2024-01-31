80c2dc30684e9564693a2c81abf139e9b77c2c3a
hericlibong <hericlibong@gmail.com>
2024-01-31 19:38:27 +0100

    add read me file, updated main.py & data_saver.py

--- BOOKS-TO-SCRAPE/data_saver.py
+++ BOOKS-TO-SCRAPE/data_saver.py
@@ -1,5 +1,5 @@
 import csv
-
+import os
 
 def save_to_csv(data_list, filename):
     """Sauvegarde une liste de dictionnaires dans un fichier CSV.
@@ -13,10 +13,44 @@ def save_to_csv(data_list, filename):
         return
 
     # Détermine les noms des champs à partir du premier élément de la liste
-    #fieldnames = data_list[0].keys()
-
     with open(filename, 'w', encoding='utf-8', newline='') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=data_list[0].keys())
         writer.writeheader()
         for data in data_list:
+            writer.writerow(data)
+
+# def save_to_csv_by_category(data_list, category):
+#     filename = f"{category}.csv"
+#     with open(filename, mode='a', newline='', encoding='utf-8') as csv_category:
+#         writer = csv.DictWriter(csv_category, fieldnames=data_list[0].keys())
+#         if csv_category.tell() == 0:
+#             writer.writeheader()
+#             for data in data_list:  
+#                 writer.writerow(data)
+            
+
+def save_to_csv_by_category(data_list, category, directory='categories_data'):
+    """Sauvegarde une liste de dictionnaires dans un fichier CSV par catégorie.
+
+    Crée un fichier CSV pour chaque catégorie et ajoute les données. Si le fichier
+    n'existe pas, il est créé et l'en-tête est ajouté. Si le fichier existe déjà,
+    les données sont ajoutées sans répéter l'en-tête.
+
+    Args:
+        data_list (list): Liste de dictionnaires contenant les données à sauvegarder.
+        category (str): Nom de la catégorie pour nommer le fichier CSV.
+        directory (str, optional): Dossier de destination des fichiers CSV. Par défaut à 'categories_data'.
+    """
+    # Assure-toi que le dossier existe, sinon crée-le
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    # Construit le chemin complet du fichier
+    filename = os.path.join(directory, f"{category}.csv")
+    
+    with open(filename, mode='a', newline='', encoding='utf-8') as csv_category:
+        writer = csv.DictWriter(csv_category, fieldnames=data_list[0].keys())
+        if csv_category.tell() == 0:  # Écrit l'en-tête si le fichier est nouveau
+            writer.writeheader()
+        for data in data_list:  
             writer.writerow(data)
--- BOOKS-TO-SCRAPE/main.py
+++ BOOKS-TO-SCRAPE/main.py
@@ -1,6 +1,6 @@
 from data_extraction import*
 from urllib.parse import urljoin
-from data_saver import save_to_csv
+from data_saver import save_to_csv, save_to_csv_by_category
 
 url = 'https://books.toscrape.com/'
 
@@ -13,40 +13,44 @@ def get_category_links(start_url):
    
     return category_list[1:]
 
-data_list = []
 categories = get_category_links(url)
+
 for categorie_url in categories:
+    # Réinitialise data_list pour chaque nouvelle catégorie
+    data_list = []
+    
     while True:
-            soup = get_soup(categorie_url)
-            book_links = soup.select('h3 a')
-            for book in book_links:
-                book_url = urljoin(categorie_url, book['href'])
-                book_soup = get_soup(book_url)
-                data = {
-                    'product_page_url': book_url,
-                    'universal_product_code': get_universal_product_code(book_soup),
-                    'title': get_title(book_soup),
-                    'price_including_tax': get_price_including_tax(book_soup),
-                    'price_excluding_tax': get_price_excluding_tax(book_soup),
-                    'number_available': get_number_available(book_soup),
-                    'product_description': get_product_description(book_soup),
-                    'category': get_category(book_soup),
-                    'star_rating': get_star_rating(book_soup),
-                    'review_rating':get_review_rating(book_soup),
-                    'image_url': get_image_url(book_soup)
-                
-                }
-                print(data)
-                data_list.append(data)
-                
-     
-            
-            # Gestion de la pagination
-            next_button = soup.find(class_='next')
-            if next_button:
-                next_page_partial = next_button.find('a')['href']
-                categorie_url = urljoin(categorie_url, next_page_partial)
-            else:
-                break
+        soup = get_soup(categorie_url)
+        book_links = soup.select('h3 a')
+        for book in book_links:
+            book_url = urljoin(categorie_url, book['href'])
+            book_soup = get_soup(book_url)
+            data = {
+                'product_page_url': book_url,
+                'universal_product_code': get_universal_product_code(book_soup),
+                'title': get_title(book_soup),
+                'price_including_tax': get_price_including_tax(book_soup),
+                'price_excluding_tax': get_price_excluding_tax(book_soup),
+                'number_available': get_number_available(book_soup),
+                'product_description': get_product_description(book_soup),
+                'category': get_category(book_soup),
+                'star_rating': get_star_rating(book_soup),
+                'review_rating': get_review_rating(book_soup),
+                'image_url': get_image_url(book_soup)
+            }
+            print(data)
+            data_list.append(data)
+        
+        # Sauvegarde les données de la catégorie actuelle une fois tous les livres de la page (et potentiellement de la pagination) traités
+        save_to_csv_by_category(data_list, get_category(book_soup))
+        
+        # Gestion de la pagination
+        next_button = soup.find(class_='next')
+        if next_button:
+            next_page_partial = next_button.find('a')['href']
+            categorie_url = urljoin(categorie_url, next_page_partial)
+        else:
+            break
+
 # Sauvegarde en dehors des boucles pour éviter les écritures multiples           
-save_to_csv(data_list, 'all_data.csv' )
+#save_to_csv(data_list, 'all_data.csv')
